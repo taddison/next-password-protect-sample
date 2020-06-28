dbb94ed8312902444eab947e0597ce79a3e2fbab
Tim Addison <git@tjaddison.com>
2020-06-28 13:09:11 -0400

    update default font to Inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
+// eslint-disable-next-line
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   variants: {},
   plugins: [require('@tailwindcss/ui')],
